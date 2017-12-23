48e9ef61be61567cec829b163986530202c95570
yasarfaruk <bprlab@10.201.191.213>
2017-12-23 13:51:09 +0300

    huseyin

--- MatematikUygulamasi/src/test/java/HesaplamaTest.java
+++ MatematikUygulamasi/src/test/java/HesaplamaTest.java
@@ -5,6 +5,33 @@ import org.junit.Test;
 public class HesaplamaTest {
 
 	@Test
+	public void testClass() {
+		boolean hataOlustu = false;
+		try {
+			Hesaplama hesaplama = new Hesaplama();
+		} catch(Exception e) {
+			hataOlustu = true;
+		}
+		
+		assertFalse(hataOlustu);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported1() {
+		Hesaplama.topla(Integer.MAX_VALUE+1, 3);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported2() {
+		Hesaplama.topla(3, Integer.MAX_VALUE+1);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported3() {
+		Hesaplama.topla(Integer.MAX_VALUE+1, Integer.MAX_VALUE+1);
+	}
+	
+	@Test
 	public void testToplaHepsi() {
 		
 		for (int sayi1=-10000; sayi1<10001; sayi1++) {
@@ -16,26 +43,35 @@ public class HesaplamaTest {
 		
 	}
 	
+	@Test
+	public void testIslemler() {
+		int gercekSonuc = Hesaplama.islemler(19,25);
+		assertEquals(10, gercekSonuc);
+	}
+
 	
 	@Test
 	public void testTopla25ve14() {
 		int gercekSonuc = Hesaplama.topla(25, 14);
 		assertEquals(39, gercekSonuc);
 	}
-	@Test
-	public void testTopla10ve20() {
-		int gercekSonuc1 = Hesaplama.topla(10, 20);
-		assertEquals(30, gercekSonuc1);
-	}
-	@Test
-	public void testCarpma1ve2() {
-		int gercekSonuc2 = Hesaplama.carpma(1, 2);
-		assertEquals(3, gercekSonuc2);
-	}
+	
 	@Test
 	public void testToplaNegatif() {
 		int gercekSonuc = Hesaplama.topla(-25, -14);
 		assertEquals(-39, gercekSonuc);
+	}
+	
+	@Test
+	public void testCarp6ve7() {
+		int gercekSonuc = Hesaplama.carpma(6, 7);
+		assertEquals(42, gercekSonuc);
+	}
+	
+	@Test
+	public void test60Bol10() {
+		int gercekSonuc = Hesaplama.bolme(60, 10);
+		assertEquals(6, gercekSonuc);
 	}
 
 }
